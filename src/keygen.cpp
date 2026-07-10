5c8ae4c23efdbb38b22470c85fbc21f823609d28
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-07-11 01:55:54 +0200

    simple keygen exec

--- src/keygen.cpp
+++ src/keygen.cpp
@@ -1,19 +1,75 @@
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+#include <charconv>
+#include <optional>
 #include <span>
 #include <string_view>
 #include <vector>
 #include <print>
 
 #include "rsa.hpp"
+#include "bigint.hpp"
 
-std::span<std::string_view> cmdline_args(int argc, const char* argv[]){
+std::span<std::string_view> cmdline_args(int argc, const char* argv[]) {
     static std::vector<std::string_view> args(argv, argv + argc);
     return args;
 }
 
-int main(int argc, const char* argv[]){
-    auto args = cmdline_args(argc, argv);
-    for (auto const& a: args){
-        std::println("{}", a);
+std::optional<std::size_t> parse_input(std::span<std::string_view> args) {
+    auto iter = std::find(args.begin(), args.end(), "-s");
+    if (iter == args.end()) {
+        std::println("Error! A prime size in bits must be specified with the -s flag");
+        return std::nullopt;
     }
+
+    ++iter;
+    if (iter == args.end()) {
+        std::println("Error! Expected an integral value after the -s flag");
+        return std::nullopt;
+    }
+
+    std::size_t value = 0;
+
+    auto str_begin = iter->data();
+    auto str_end = str_begin + iter->size();
+    auto [ptr, ec] = std::from_chars(str_begin, str_end, value);
+    if (ec != std::errc{} or ptr != str_end) {
+        std::println("Error! Could not parse {} as a valid prime size", *iter);
+        return std::nullopt;
+    }
+
+    return value;
+}
+
+void dump_key_file(std::string_view n, std::string_view key, std::filesystem::path file) {
+    std::ofstream out(file);
+    if (!out) {
+        std::println("Error! Could not write file {}", std::string{file.filename()});
+        return;
+    }
+
+    out << n << '\n';
+    out << key << '\n';
+}
+
+int main(int argc, const char* argv[]) {
+    auto args = cmdline_args(argc, argv);
+    auto opt_size = parse_input(args);
+    if (not opt_size) {
+        return 1;
+    }
+    auto opt_key = rsa::keygen<rsa::bigint<32>>(*opt_size);
+    if (not opt_key) {
+        std::println("Error! Requested prime size is too large");
+        return 2;
+    }
+
+    std::string n = opt_key->n.to_string();
+    std::string pub =  opt_key->pub.to_string();
+    std::string prv =  opt_key->prv.to_string();
+    std::println("n={} pub={} prv={}", n, pub, prv);
+    dump_key_file(n, pub, "rsa.pub");
+    dump_key_file(n, prv, "rsa.prv");
     return 0;
 }
